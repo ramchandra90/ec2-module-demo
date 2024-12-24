7a3642609d30b79f29b42614592c5ba8ad2e9f42
ramachandra <ramchandra081@gmail.com>
2024-12-24 15:04:43 +1300

    functions

--- provider.tf
+++ provider.tf
@@ -1,14 +1,17 @@
-terraform {
-    required_providers {
-        aws {
+terraform{
+    required_providers{
+        aws = {
             source = "hashicorp/aws"
             version = "5.66.0"
 
         }
-
     }
 }
 
-provider aws {
+provider "aws"{
     region = "us-east-1"
 }
+
+
+
+
